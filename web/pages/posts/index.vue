68bd512b8dadd0a6b962984f9acf7a11bfd637f8
yi.cai@haku-mata.cn <yi.cai@haku-mata.cn>
2017-11-30 18:15:46 +0800

    delete

---
+++ web/pages/posts/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        posts
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'blog'
+}
+</script>
+
+<style>
+
+</style>
